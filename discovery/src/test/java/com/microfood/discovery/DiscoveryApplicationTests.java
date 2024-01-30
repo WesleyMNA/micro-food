f91415c769650240d2d6e366f33ade6d52b1e490
Wesley Almeida <wesley.almeida@fotosensores.com>
2024-01-30 14:48:52 -0300

    discovery added

---
+++ discovery/src/test/java/com/microfood/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microfood.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
