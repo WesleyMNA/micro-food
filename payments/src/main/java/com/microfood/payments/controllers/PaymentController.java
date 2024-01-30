6b73bb36b795fa7fcf7c1ffc2f11866f8464d927
Wesley Almeida <wesley.almeida@fotosensores.com>
2024-01-30 12:01:18 -0300

    PaymentController added

---
+++ payments/src/main/java/com/microfood/payments/controllers/PaymentController.java
@@ -0,0 +1,54 @@
+package com.microfood.payments.controllers;
+
+import com.microfood.payments.dtos.PaymentDto;
+import com.microfood.payments.services.PaymentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/v1/payments")
+public class PaymentController {
+
+    private final PaymentService service;
+
+    @GetMapping
+    public ResponseEntity<Page<PaymentDto>> findAll(Pageable pageable) {
+        Page<PaymentDto> response = service.findAll(pageable);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<PaymentDto> create(@RequestBody @Valid PaymentDto dto,
+                                             UriComponentsBuilder builder) {
+        PaymentDto response = service.create(dto);
+        URI uri = builder.path("/v1/payments/{id}").buildAndExpand(response.getId()).toUri();
+        return ResponseEntity
+                .created(uri)
+                .body(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@PathVariable Long id,
+                                       @RequestBody @Valid PaymentDto dto) {
+        service.update(id, dto);
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity
+                .noContent()
+                .build();
+    }
+}
