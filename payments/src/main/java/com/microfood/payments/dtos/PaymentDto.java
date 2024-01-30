87f7fbb3e42beffaeef4ba5a58ffba5935083ac9
Wesley Almeida <wesley.almeida@fotosensores.com>
2024-01-30 12:01:31 -0300

    validation added to PaymentDto

--- payments/src/main/java/com/microfood/payments/dtos/PaymentDto.java
+++ payments/src/main/java/com/microfood/payments/dtos/PaymentDto.java
@@ -1,6 +1,9 @@
 package com.microfood.payments.dtos;
 
 import com.microfood.payments.models.Status;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,11 +14,18 @@ import java.math.BigDecimal;
 public class PaymentDto {
 
     private Long id;
+    @NotNull
+    @Positive
     private BigDecimal value;
+    @NotBlank
     private String name;
+    @NotBlank
     private String expiration;
+    @NotBlank
     private String code;
     private Status status;
+    @NotNull
     private Long orderId;
+    @NotNull
     private Long paymentTypeIdd;
 }
